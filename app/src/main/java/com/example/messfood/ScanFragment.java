e8253c6bc4048566507502eb11b953b5cb9ddc35
Animesh Singh <csanimeshsingh747@gmail.com>
2023-03-06 00:10:10 +0530

    Minor changes

--- app/src/main/java/com/example/messfood/ScanFragment.java
+++ app/src/main/java/com/example/messfood/ScanFragment.java
@@ -8,19 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScanFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ScanFragment extends Fragment {
+import com.example.messfood.databinding.FragmentScanBinding;
+import com.google.zxing.integration.android.IntentIntegrator;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+public class ScanFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,14 +23,6 @@ public class ScanFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScanFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static ScanFragment newInstance(String param1, String param2) {
         ScanFragment fragment = new ScanFragment();
@@ -56,9 +43,9 @@ public class ScanFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scan, container, false);
+
     }
 }
