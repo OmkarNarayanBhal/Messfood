5cea6438630da6578389f8d1ca6d8d71d1756f29
Animesh Singh <csanimeshsingh747@gmail.com>
2023-03-06 01:41:11 +0530

    Scanner update

--- app/src/main/java/com/example/messfood/ScanFragment.java
+++ app/src/main/java/com/example/messfood/ScanFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.messfood.databinding.FragmentScanBinding;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -15,13 +16,12 @@ import com.google.zxing.integration.android.IntentIntegrator;
 public class ScanFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     private String mParam1;
     private String mParam2;
-
     public ScanFragment() {
         // Required empty public constructor
     }
+
 
     // TODO: Rename and change types and number of parameters
     public static ScanFragment newInstance(String param1, String param2) {
@@ -32,7 +32,6 @@ public class ScanFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +45,7 @@ public class ScanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scan, container, false);
+
 
     }
 }
