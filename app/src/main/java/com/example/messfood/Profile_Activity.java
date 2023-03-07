a7a432b01621a3e8458a3c75e21e9a2a0150539d
abhishekraj8579 <112793553+abhishekraj8579@users.noreply.github.com>
2023-03-08 02:33:33 +0530

    minor changes

--- app/src/main/java/com/example/messfood/Profile_Activity.java
+++ app/src/main/java/com/example/messfood/Profile_Activity.java
@@ -15,31 +15,31 @@ public class Profile_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
-        bottomNavigationView.setSelectedItemId(R.id.profile);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.home:
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.order:
-                        startActivity(new Intent(getApplicationContext(),Order_Activity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-//                    case R.id.scan:
-//                        startActivity(new Intent(getApplicationContext(),Scan_Activity.class));
+//        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
+//        bottomNavigationView.setSelectedItemId(R.id.profile);
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()){
+//                    case R.id.home:
+//                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
 //                        overridePendingTransition(0,0);
 //                        return true;
-                    case R.id.profile:
-                        return true;
-
-                }
-                return false;
-            }
-        });
+//                    case R.id.order:
+//                        startActivity(new Intent(getApplicationContext(),Order_Activity.class));
+//                        overridePendingTransition(0,0);
+//                        return true;
+////                    case R.id.scan:
+////                        startActivity(new Intent(getApplicationContext(),Scan_Activity.class));
+////                        overridePendingTransition(0,0);
+////                        return true;
+//                    case R.id.profile:
+//                        return true;
+//
+//                }
+//                return false;
+//            }
+//        });
     }
 }
