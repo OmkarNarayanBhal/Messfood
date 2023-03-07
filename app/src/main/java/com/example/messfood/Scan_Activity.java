fa86e32ef32669c0ae8ecba0636d8aa243ee6a13
Animesh Singh <csanimeshsingh747@gmail.com>
2023-03-08 01:45:38 +0530

    QR Code reader Activity final update

--- app/src/main/java/com/example/messfood/Scan_Activity.java
+++ app/src/main/java/com/example/messfood/Scan_Activity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
@@ -22,22 +23,16 @@ public class Scan_Activity extends AppCompatActivity {
 
 
 
-    Button btn_scan;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
-        btn_scan = findViewById(R.id.scan);
-        btn_scan.setOnClickListener(v ->
-        {
-            scanCode();
-        });
 
 
 
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
-        bottomNavigationView.setSelectedItemId(R.id.scan);
+       // bottomNavigationView.setSelectedItemId(R.id.fav_btn);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -51,9 +46,6 @@ public class Scan_Activity extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(),Order_Activity.class));
                         overridePendingTransition(0,0);
                         return true;
-                    case R.id.scan:
-                        scanCode();
-                        return true;
                     case R.id.profile:
                         startActivity(new Intent(getApplicationContext(),Profile_Activity.class));
                         overridePendingTransition(0,0);
@@ -64,29 +56,5 @@ public class Scan_Activity extends AppCompatActivity {
         });
     }
 
-    private void scanCode() {
-
-        ScanOptions options = new ScanOptions();
-        options.setPrompt("Volume up to flash on");
-        options.setBeepEnabled(true);
-        options.setOrientationLocked(true);
-        options.setCaptureActivity(CaptureAct.class);
-        barLauncher.launch(options);
-    }
-    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->
-    {
-        if (result.getContents() !=null)
-        {
-          AlertDialog.Builder builder = new AlertDialog.Builder(Scan_Activity.this);
-          builder.setTitle("Result");
-          builder.setMessage(result.getContents());
-          builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-              @Override
-              public void onClick(DialogInterface dialogInterface, int i) {
-                  dialogInterface.dismiss();
-              }
-          }).show();
-        }
-    });
 
 }
